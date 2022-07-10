056daefc2035ebe365a4f7d07eabfb57287821c3
Roman Muraro <roman.muraro@gmail.com>
2022-07-10 13:43:48 +1000

    Добавление функции факториала

---
+++ SixthTask/ManyFuctions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+int func(int a){
+     int cd = 1;
+	for(int i=1;i<=a;i++){
+		cd = cd*i;
+	}
+return cd;}
+
+int main(){
+	int b;
+	cin>>b;
+	b += 2;
+	b = func(b);
+	cout<<b;
+return 0;}
